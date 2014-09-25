cc8139a91d1158aecb47bc1d75e78d28d6879ff2
McFeod <macfeod@yandex.ru>
2014-09-25 20:29:29 +0600

    Минимальная работающая версия включено: 	быстрые роботы 	очки, энергия 	безопасные ходы 	безопасные телепорты не включено: 	управление 	отрисовка 	возможность двигать обломки

---
+++ src/com/github/mcfeod/robots4droid/SimplePlayer.java
@@ -0,0 +1,50 @@
+package com.github.mcfeod.robots4droid;
+
+public class SimplePlayer{
+    private Point mPos; //просто координаты
+    private int mScore; 
+    private int mEnergy;
+    private int mLevel;	
+
+	public SimplePlayer(int x, int y){
+        mPos.x = x;
+        mPos.y = y;
+        mEnergy = 0;
+        mScore = 0;
+    }
+    
+    public int getLevel() {
+		return mLevel;
+	}
+
+	public void incLevel() {
+		mEnergy+= mLevel*0.2+1;
+		mScore+= mLevel*5;
+		mLevel++;
+	}
+	
+    public Point getPos() {
+		return mPos;
+	}  
+
+	public void setPos(Point pos) {
+		mPos = pos;
+	}
+
+	public int getScore() {
+		return mScore;
+	}
+	
+	public void incScore(int diff){
+		mScore += diff;
+	}
+
+	public int getEnergy() {
+		return mEnergy;
+	}
+	
+	public void chEnergy(int diff){
+		mScore += diff;
+	}
+
+}
