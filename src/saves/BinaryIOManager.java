918cd7d60f0bc4455a017ff30a578ea09026b3d9
McFeod <mcfeod@gmail.com>
2014-11-01 21:48:22 +0600

    Вернул то, что сначала принял за мусор :D

--- src/saves/BinaryIOManager.java
+++ src/saves/BinaryIOManager.java
@@ -112,6 +112,8 @@ public class BinaryIOManager {
 
 	private void loadDesk(DataInput input)
 	throws IOException{
+		int x = input.readInt();
+		int y = input.readInt();
 		for (int i = 0; i < mBoard.length ; i++) {
 			input.readFully(mBoard[i]);
 		}
