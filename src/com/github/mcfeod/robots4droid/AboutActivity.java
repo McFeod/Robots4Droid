6616c99b726a9df13c5d5549a7b744c94fba119a
McFeod <macfeod@yandex.ru>
2014-12-18 21:39:45 +0500

    Благодаря ребятам из кингсофта, переделываю одно и то же...

--- src/com/github/mcfeod/robots4droid/AboutActivity.java
+++ src/com/github/mcfeod/robots4droid/AboutActivity.java
@@ -1,13 +1,18 @@
 package com.github.mcfeod.robots4droid;
 
+import java.lang.reflect.InvocationTargetException;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.ZoomButtonsController;
 
 public class AboutActivity extends Activity{
 
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 	@Override
 		protected void onCreate(Bundle savedInstanceBundle){
 			super.onCreate(savedInstanceBundle);
@@ -17,7 +22,19 @@ public class AboutActivity extends Activity{
 			settings.setDefaultTextEncodingName("utf-8");
 			content.loadUrl(getString(R.string.about_url));
 			content.setScrollbarFadingEnabled(true);
+			settings.setSupportZoom(true);
+			settings.setBuiltInZoomControls(true);
 			content.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+			if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB) {
+				settings.setDisplayZoomControls(false);
+			} else {
+				ZoomButtonsController zoom_controll;
+				try {
+					zoom_controll = (ZoomButtonsController) content.getClass().getMethod("getZoomButtonsController").invoke(content, null);
+					zoom_controll.getContainer().setVisibility(View.GONE);
+				} catch (IllegalAccessException e) {} catch (IllegalArgumentException e){} catch (InvocationTargetException e) {} catch (NoSuchMethodException e) {}
+				
+			}
 	}
 
 }
